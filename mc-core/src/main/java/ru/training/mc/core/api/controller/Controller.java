207c83e2fda9724ceb29fdac79aa81ba70f0ff7c
Mikhail Semenov <semenovmikhailal@gmail.com>
2023-12-23 17:59:11 +0300

    Add controller for core

---
+++ mc-core/src/main/java/ru/training/mc/core/api/controller/Controller.java
@@ -0,0 +1,45 @@
+package ru.training.mc.core.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.training.mc.core.api.service.MonitoringService;
+
+@RestController
+@RequestMapping("/api/monitor/current")
+@AllArgsConstructor
+public class Controller {
+
+    private final MonitoringService monitoringService;
+
+    @GetMapping("/temperature/{unit}")
+    public ResponseEntity<Float> readTemperature(@PathVariable String unit) {
+        return switch (unit.toLowerCase()) {
+            case ("c") -> ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(monitoringService.getTemperatureInC());
+            case ("f") -> ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(monitoringService.getTemperatureInF());
+            default -> ResponseEntity.notFound().build();
+        };
+    }
+
+    @GetMapping("/pressure/{unit}")
+    public ResponseEntity<Float> readPressure(@PathVariable String unit) {
+        return switch (unit.toLowerCase()) {
+            case ("mmhg") -> ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(monitoringService.getPressureInMmHg());
+            case ("pa") -> ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(monitoringService.getPressureInPa());
+            default -> ResponseEntity.notFound().build();
+        };
+    }
+
+}
