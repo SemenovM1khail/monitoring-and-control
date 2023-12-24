4d2213ab49e159b692067b916b522f074a5a2290
Mikhail Semenov <semenovmikhailal@gmail.com>
2023-12-24 22:22:34 +0300

    Add dockefile and main app test

---
+++ mc-core/src/test/java/ru/training/mc/core/McCoreApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.training.mc.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
